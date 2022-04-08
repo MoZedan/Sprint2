33dd2e9aa6fe9c548da955d7df52ffe9fbc2d4a6
MoZedan <70353945+MoZedan@users.noreply.github.com>
2022-04-08 02:34:26 +0200

    Update Task 2.py

--- Task 2.py
+++ Task 2.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
 
 # # Question 1:
 # Define a function which can print a dictionary where the keys are numbers between 1 and 3 (both included) and the values are square of keys.
@@ -19,35 +17,7 @@ def printDict():
 printDict()
 
 
-# # Question 2:
-# Define a function which can generate a dictionary where the keys are numbers between 1 and 20 (both included) and the values are square of keys. The function should just print the values only.
-# 
-# Hints:
-# 
-# Use dict[key]=value pattern to put entry into a dictionary.
-# Use ** operator to get power of a number.
-# Use range() for loops.
-# Use keys() to iterate keys in the dictionary. Also we can use item() to get key/value pairs.
-# 
 
-# In[ ]:
-
-
-
-
-
-# # Question 3:
-# Define a function which can generate a dictionary where the keys are numbers between 1 and 20 (both included) and the values are square of keys. The function should just print the keys only.
-# 
-# Hints:
-# 
-# Use dict[key]=value pattern to put entry into a dictionary.
-# Use ** operator to get power of a number.
-# Use range() for loops.
-# Use keys() to iterate keys in the dictionary. Also we can use item() to get key/value pairs.
-# 
-
-# In[ ]:
 
 
 
@@ -65,26 +35,9 @@ if not bool(my_dict):
     print("Dictionary is empty")
 
 
-# # Question 5
-# 
-# Write a Python program to test whether every element in s is in t and every element in t is in s.
-# 
-# 
-
-# In[ ]:
 
 
 
-
-
-# # Question 6
-# Create set 1 and set 2 then 
-# Write a Python program to 1- create an intersection of sets.
-# 2-  a union of sets.
-# 3- create set difference.
-# 4- a symmetric difference.
-
-# In[ ]:
 
 
 
@@ -145,45 +98,9 @@ print(tp1)
 print(tp2)
 
 
-# # Question 10 :
-# You are required to write a program to sort the (name, age, height) tuples by ascending order where name is string, age and height are numbers. The tuples are input by console. The sort criteria is:
-# 1: Sort based on name;
-# 2: Then sort based on age;
-# 3: Then sort by score.
-# The priority is that name > age > score.
-# If the following tuples are given as input to the program:
-# Tom,19,80
-# John,20,90
-# Jony,17,91
-# Jony,17,93
-# Json,21,85
-# Then, the output of the program should be:
-# [('John', '20', '90'), ('Jony', '17', '91'), ('Jony', '17', '93'), ('Json', '21', '85'), ('Tom', '19', '80')]
-# 
-# Hints:
-# In case of input data being supplied to the question, it should be assumed to be a console input.
-# We use itemgetter to enable multiple sort keys.
-# 
-# Solutions:
-# from operator import itemgetter, attrgetter
-# 
-
-# In[ ]:
 
 
 
-
-
-# # Question 11:
-# 
-# Please write a program to generate a list with 5 random numbers between 100 and 200 inclusive.
-# 
-# 
-# 
-# Hints:
-# Use random.sample() to generate a list of random values.
-
-# In[8]:
 
 
 
@@ -217,14 +134,7 @@ x = random.randint(7,15)
 print(x)
 
 
-# # Question 14
-# 
-# Please write a program to print the running time of execution of "1+1" for 100 times.
-# Hints:
-# Use timeit() function to measure the running time.
-# 
 
-# In[ ]:
 
 
 
